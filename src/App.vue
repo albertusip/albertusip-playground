ec679128661beab557443f84666ef501b7113a6f
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2020-10-20 17:27:03 +0700

    add data table

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,174 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-app-bar
+        app
+        color="primary"
+        dark
+        >
+        <div class="d-flex align-center">
+            Albertusip Playground
+        </div>
+        </v-app-bar>
+
+        <v-main>
+            <v-container class="mt-5">
+                <v-card>
+                    <v-card-title
+                        class="pb-0"
+                    >
+                        <v-row justify="space-between">
+                            <v-col
+                                cols="12"
+                                sm="6"
+                            >
+                                <v-text-field
+                                    v-model="search"
+                                    autocomplete="off"
+                                    clearable
+                                    append-icon="mdi-magnify"
+                                    label="Search"
+                                    single-line
+                                    hide-details
+                                />
+                            </v-col>
+                        </v-row>
+                    </v-card-title>
+                    <v-data-table
+                        :headers="headers"
+                        :items="tableItems"
+                        :options.sync="options"
+                        :page.sync="page"
+                        :server-items-length="totalTableItems"
+                        :loading="loadingTable"
+                        :footer-props="{
+                            itemsPerPageText: 'Data per halaman',
+                            itemsPerPageOptions: [5,10,15,20,50,100]
+                        }"
+                    >
+
+                        <template v-slot:body="{ items }">
+                            <tbody v-if="items.length > 0">
+                                <tr
+                                    v-for="(item, index) in items"
+                                    :key="item.name"
+                                >
+                                    <td>{{ getNumberRows(index) }}</td>
+                                    <td>{{ item.fullname ? item.fullname : 'Tidak Diketahui' }}</td>
+                                    <td>
+                                        {{`
+                                            ${item.birthplace ? item.birthplace : 'Tidak Diketahui'}, 
+                                            ${item.birthdate ? formatDate(item.birthdate) : 'Tidak Diketahui'}
+                                        ` }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <tbody v-else>
+                                <tr>
+                                <td :colspan="headers.length" style="text-align: center">Tidak ada Data</td>
+                                </tr>
+                            </tbody>
+                        </template>
+
+                    </v-data-table>
+                    <v-card-text class="text-center pt-0">
+                        <v-row class="d-flex justify-center">
+                            <v-col
+                                cols="7"
+                                sm="6"
+                                md="5"
+                                lg="4"
+                            >
+                                <v-pagination
+                                    v-model="page"
+                                    :disabled="loadingTable"
+                                    :length="totalPage"
+                                    circle
+                                />
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                </v-card>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const SEARCH_DEBOUNCE_MS = 500;
+
+import debounce from 'lodash/debounce';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    data: () => ({
+        page: 1,
+        totalPage: 1,
+        totalTableItems: 0,
+        tableItems: [],
+        loadingTable: true,
+        options: {
+            page: 1,
+            rowsPerPage: 10,
+            sortBy: [],
+            sortDesc: []
+        },
+        headers: [
+            { text: 'No', align: 'start', value: 'index', sortable: false },
+            { text: 'Name', align: 'start', value: 'fullname', sortable: false },
+            { text: 'Birthday', value: 'birth_date_place', sortable: false },
+        ],
+        search: null
+    }),
+    watch: {
+        options: {
+            handler () {
+                this.loadingTable = true;
+                this.getDataFromApi();
+            },
+            deep: true
+        },
+        search () {
+            this.loadingTable = true;
+            this.getDataFromApi();
+        }
+    },
+    
+    async mounted () {
+        await Promise.all([
+            this.getDataFromApi()
+        ]);
+    },
+    methods: {
+        formatDate (s) {
+            const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Octtober', 'November', 'December'];
+            s = s.replace(/-/g, '/');
+            const d = new Date(s);
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+            return d.getDate() + ' ' + months[d.getMonth()] + ' ' + d.getFullYear();
+        },
+        getNumberRows (index){
+            return ((this.page-1) * this.options.itemsPerPage) + index + 1;
+        },
+        getDataFromApi: debounce (async function () {
+            const { page, itemsPerPage } = this.options;
+
+            try {
+                this.loadingTable = true;
+                const result = await this.$axios.post('https://api-hangman.jpcc.my.id/api/user/filtered', {
+                    paginate: itemsPerPage,
+                    page: page,
+                    search: this.search
+                });
+
+                this.tableItems = result.data && result.data.data;
+                this.totalTableItems = result.data && result.data.total;
+                this.totalPage = result.data && result.data.last_page;
+                this.page = result.data && result.data.current_page;
+            } catch (error) {
+                console.error(error);
+            } finally {
+                this.loadingTable = false;
+            }
+        }, SEARCH_DEBOUNCE_MS)
+    }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+window.axios = axios.create();
+Vue.prototype.$axios = window.axios;
+
+window.axios.defaults.headers.common = {
+    Accept: 'application/json'
+};
 
 new Vue({
-  render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
